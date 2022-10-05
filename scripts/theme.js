e34d2ca7d24ee030461ae4086aa67dd7f0231491
Valdecir Teixeira <vardeba@hotmail.com>
2022-10-05 01:11:52 -0300

    feat: dark mode working

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,3 +1,21 @@
-function handleDarkMode(){
-    const darkModeButton = document.querySelector('.mode-switch')
+function darkModeSwitch(){
+    const darkModeButton = document.querySelector('.mode-switch');
+    const html = document.querySelector('html');
+
+    darkModeButton.addEventListener('click', () => {
+        html.classList.toggle('dark-mode');
+        darkModeButton.classList.toggle('light-icon');
+        darkModeButton.classList.toggle('dark-icon');
+    });
+
+    const darkModeFix = document.localStorage.getItem('darkmode');
+
+    if (!darkModeFix){
+        localStorage.setItem('darkmode', true);
+    }
+    if (darkModeFix){
+        localStorage.removeItem('darkmode');
+    }
 }
+
+darkModeSwitch();
